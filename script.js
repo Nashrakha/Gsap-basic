8720bf5d2af5fec5dcf043b4391d82a68345c099
Nashrakha <nashrak562@gmail.com>
2024-07-01 18:04:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// gsap.to("#box1",{
+//     x:500,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     backgroundColor:"yellow",
+//     borderRadius:"50px",
+//     scale:0.5
+// }) 
+// gsap.from("#box2",{
+//     x:1000,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     backgroundColor:"yellow"
+// }) 
+// gsap.from("h1",{
+//     opacity:0,
+//     duration:2,
+//     y:30,
+//     delay:1,
+//     color:"red",
+//     stagger:0.3
+// }) 
+// gsap.to("#box1",{
+//     x:1000,
+//     duration:1,
+//     delay:1,
+//     rotate:360,
+//     repeat:-1,
+//     yoyo:true,
+//     backgroundColor:"yellow",
+//     borderRadius:"50px",
+//     scale:0.5
+// }) 
+
+// timeline 
+var tl = gsap.timeline()
+tl.to("#box1", {
+    x: 1000,
+    duration: 1,
+    delay: 1,
+    rotate: 360,
+})
+tl.to("#box2", {
+    x: 1000,
+    duration: 1,
+    delay: 1,
+    backgroundColor: "white"
+})
